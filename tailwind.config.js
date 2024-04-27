6fa2eba24670eecae39f558e0623d37044b07686
085 Muhammad Abubakar <abubakarnawaz032@gmail.com>
2024-04-27 15:23:25 +0500

    sam update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        // "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        // "gradient-conic":
-        //   "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
         "primary": "#111111",
         "span":  "#00F0FF", 
-        "secondary" :"#3A3A3A4D"
+        "secondary" :"#3A3A3A4D",
+        "span2" : "linear-gradient(90deg, #FF1CF7 0%, #00F0FF 100%)"
       },
       
       fontFamily: {
