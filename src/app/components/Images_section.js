98a2190216015a3df2d00773503d231aaa7d5d32
085 Muhammad Abubakar <abubakarnawaz032@gmail.com>
2024-04-27 17:08:15 +0500

    update

--- src/app/components/Images_section.js
+++ src/app/components/Images_section.js
@@ -5,7 +5,7 @@ const Images_section = () => {
   return (
     <div className='w-[90%] lg:w-[75%] m-auto'>
       <p className=" text-purple-600 text-center   ">Powering tools and integrations from companies all around the world</p>
-      <div className='grid grid-cols-7 gap-5 mt-10'>
+      <div className='grid grid-cols-4 lg:grid-cols-7 md:grid-cols-5 gap-5 mt-10'>
       {
         images.map((curentItem, index)=>{
             return <div key={index}><img src={curentItem.url} alt={curentItem.id} /></div>
